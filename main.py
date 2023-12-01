61365701a25cab9579a23dfe2d838d481a6c376d
Mingyang <mliu16@binghamton.edu>
2023-12-01 13:09:40 -0500

    message

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pygame
 
 def main():
     pygame.init()
+screen = pygame.display.set_mode((600,600))
+screen_size = pygame.display.get_window_size()
+width, height = screen_size[0], screen_size[1]
     #Create an instance on your controller object
     #Call your mainloop
     
@@ -11,3 +14,4 @@ def main():
 # https://codefather.tech/blog/if-name-main-python/
 if __name__ == '__main__':
     main()
+
