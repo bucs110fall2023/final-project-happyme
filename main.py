d2c01ce19df097fe02f1092c85cd9345bb6513b8
Your Namefrogmoogan <meng5@binghamton.edu>
2023-12-01 14:42:06 -0500

    attempt

--- main.py
+++ main.py
@@ -1,13 +1,54 @@
 import pygame
 #import your controller
 
+#variables
+player_velocity = 10
+player_width = 40
+player_height = 60
+
+
+
+def check_input (key):
+    if key == pygame.K_LEFT:
+        player_x -= player_velocity
+    elif key == pygame.K_RIGHT:
+        player_x += player_velocity
+    elif key == pygame.K_DOWN:
+        player_y += player_velocity
+    elif key == pygame.K_UP:
+        player_y -= player_velocity
+                    
+def mainloop(pygame):
+    player_x = 30 
+    player_y = -30 
+    run = True
+    screen = pygame.display.set_mode((1200, 720)) 
+    pygame.display.set_caption("Museum Game")
+        
+    while run: 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        if event.type == pygame.KEYDOWN: 
+            check_input((event.key))
+                        
+        screen.fill("WHITE")
+        pygame.draw.rect(screen, ("PURPLE"), (player_x, player_y, player_width, player_height ))
+        pygame.display.update()
+    
+       
+
 def main():
     pygame.init()
-    #Create an instance on your controller object
-    #Call your mainloop
     
+    #Create an instance on your controller object
+    mainloop (pygame) #Call your mainloop
     ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
 
 # https://codefather.tech/blog/if-name-main-python/
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+    #main()
+
+main()
+
+ 
